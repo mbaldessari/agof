34084d9341526884286ab91cc80f19c98c6df547
Michele Baldessari <michele@acksyn.org>
2026-02-26 18:58:39 +0100

    trynewcontainer

--- scripts/pattern-util.sh
+++ scripts/pattern-util.sh
@@ -9,7 +9,8 @@ function version {
 }
 
 if [ -z "$PATTERN_UTILITY_CONTAINER" ]; then
-	PATTERN_UTILITY_CONTAINER="quay.io/hybridcloudpatterns/utility-container:v1.0.2"
+	#PATTERN_UTILITY_CONTAINER="quay.io/hybridcloudpatterns/utility-container:v1.0.2"
+	PATTERN_UTILITY_CONTAINER="quay.io/rhn_support_mbaldess/utility-container:ansible-2.20"
 fi
 # If PATTERN_DISCONNECTED_HOME is set it will be used to populate both PATTERN_UTILITY_CONTAINER
 # and PATTERN_INSTALL_CHART automatically
